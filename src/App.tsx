398886e44f645ea29f7024df5bfdc2eee6cff09a
Max Frisson <max.chebonenko@gmail.com>
2021-07-01 00:25:37 +0300

    fix CounterOutput

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import style from "./App.module.css";
 import { Counter } from "./components/Counter/Counter";
 import { Decremenator } from "./components/Decremenator/Decremenator";
+import { Greetings } from "./components/Greetings/Greetings";
 import { Incremenator } from "./components/Incremenator/Incremenator";
 
 const App: React.FC = () => {
   return (
     <div className={style.appBody}>
-      <Counter />
-      <Incremenator />
-      <Decremenator />
+      <div className={style.appGreetings}>
+        <Greetings />
+      </div>
+      <div className={style.appList}>
+        <Counter />
+        <Incremenator />
+        <Decremenator />
+      </div>
     </div>
   );
 };
---
+++ src/components/Greetings/Greetings.tsx
@@ -0,0 +1,7 @@
+export const Greetings = () => {
+  return (
+    <>
+      <h1>Hello to my react-test-app</h1>
+    </>
+  );
+};

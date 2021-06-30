8c4567963b3aeab52b37c7eaf11c64f6f079d08b
Max Frisson <max.chebonenko@gmail.com>
2021-06-30 23:43:58 +0300

    add Incrementator

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import style from "./App.module.css";
 import { Counter } from "./components/Counter/Counter";
+import { Incremenator } from "./components/Incremenator/Incremenator";
 
 const App = () => {
   return (
     <div className={style.appBody}>
       <Counter />
+      <Incremenator />
     </div>
   );
 };
